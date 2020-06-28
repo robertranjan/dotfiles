c0e7c72106eed87449c8ddd7102de13ab8292fe0
Robert Thanulingam <rranjan3@apple.com>
2020-06-28 00:04:42 -0700

    run as a bash script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -k https://raw.githubusercontent.com/robertranjan/dotfiles/master/bashrc >> ~/.bashrc
 curl -k https://raw.githubusercontent.com/robertranjan/dotfiles/master/tmux.conf > ~/.tmux.conf
 curl -k https://raw.githubusercontent.com/robertranjan/dotfiles/master/vimrc >> ~/.vimrc
